d0fe83ba63281b5318937eb974725ad0b535baf3
Marco Pacchiotti <ICTS22-24.016@studenti.local>
2023-04-11 17:56:14 +0200

    Dadi

---
+++ JavaScript/Lez01/script.js
@@ -0,0 +1,12 @@
+//dadi
+const Dado1 = document.getElementById('Dado1')
+const Dado2 = document.getElementById('Dado2')
+let random = parseInt(Math.random() * 6) + 1;
+let random2 = parseInt(Math.random() * 6) + 1;
+console.log(random);
+console.log(random2);
+random = 'https://raw.githubusercontent.com/maboglia/ProgrammingResources/master/images/dadi/f' + random + '.jpg';
+random2 = 'https://raw.githubusercontent.com/maboglia/ProgrammingResources/master/images/dadi/f' + random2 + '.jpg';
+Dado1.src = random;
+Dado2.src = random2;
+Math.random()
