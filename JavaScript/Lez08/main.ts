84d60fce61c06746ef24be650659e9eb83801761
MarcoPacchiotti <57013700+pacchio1@users.noreply.github.com>
2023-09-26 11:40:22 +0200

    typescript + lez 08

---
+++ JavaScript/Lez08/main.ts
@@ -0,0 +1,14 @@
+/*let num: number;
+let b: boolean;
+let c: string;
+let d: any;
+
+let voti: number[] = [1, 2, 3, 4, 5];
+let generic: any[] = ['pippo', 30, true];
+enum Color { Rosso = 0, Verde = 2, Blue = 4 };
+
+console.log(Color.Rosso)
+*/
+/* let msg;
+msg = 'pippo';
+console.log((msg as string).toUpperCase); */
